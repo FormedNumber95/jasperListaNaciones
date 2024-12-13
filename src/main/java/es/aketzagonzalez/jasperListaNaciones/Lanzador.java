9567e776be4191d587820401dcdf03dbb01f0159
FormedNumber95 <aketza200510@gmail.com>
2024-12-13 17:13:36 +0100

    añadido javadoc y cambiado el nombre a la clase principal

--- src/main/java/es/aketzagonzalez/jasperListaNaciones/Lanzador.java
+++ src/main/java/es/aketzagonzalez/jasperListaNaciones/Lanzador.java
@@ -13,20 +13,30 @@ import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.view.JasperViewer;
 
-public class MainApp {
+/**
+ * Clase que al ejecutarse lanza el reporte de jaspwer.
+ */
+public class Lanzador {
    
+    /**
+     * Metodo main que lanza el reporte y le da al valor IMAGE_PATH la ruta de las imagenes dentro del proyecto.
+     *
+     * @param args the arguments
+     */
     public static void main(String[] args) {
         ConexionBBDD db;
         try {
             db = new ConexionBBDD();
             // Cargar el reporte desde la ruta
             InputStream reportStream = db.getClass().getResourceAsStream("/jasper/PaisesUD3_ej1.jasper");
+            /*
             if (reportStream == null) {
                 System.out.println("El archivo no está ahí");
                 return;
             } else {
                 System.out.println("El archivo se ha encontrado");
             }
+            */
             JasperReport report = (JasperReport) JRLoader.loadObject(reportStream);
             Map<String, Object> parameters = new HashMap<>();
             parameters.put("IMAGE_PATH", db.getClass().getResource("/imagenes/").toString());
